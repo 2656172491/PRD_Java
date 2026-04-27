5bdf73f832ceb34b40ba01bbe0841111aa873ce1
2656172491 <2656172491@qq.com>
2026-04-27 10:25:45 +0800

    初始化 Spring Boot 3.2 项目，集成 MyBatis-Plus、H2 数据库、SpringDoc OpenAPI，配置数据库表结构

---
+++ src/main/java/com/prd/PersonalRelationshipApplication.java
@@ -0,0 +1,11 @@
+package com.prd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalRelationshipApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalRelationshipApplication.class, args);
+    }
+}
